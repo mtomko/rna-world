d65773a69255420552888081fe5eeca2627bc996
Mark Tomko <mark.tomko.1@gmail.com>
2021-12-21 08:29:54 +0100

    Add errors module

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,16 @@
 use actix_web::{HttpResponse, ResponseError};
+use deadpool_postgres::PoolError;
 use derive_more::{Display, From};
+use tokio_pg_mapper::Error as PGMError;
+use tokio_postgres::error::Error as PGError;
 
 #[derive(Display, From, Debug)]
+#[allow(clippy::enum_variant_names)]
 pub enum RWError {
     CsvError,
+    PGError(PGError),
+    PGMError(PGMError),
+    PoolError(PoolError),
 }
 impl std::error::Error for RWError {}
 
@@ -11,6 +18,11 @@ impl ResponseError for RWError {
     fn error_response(&self) -> HttpResponse {
         match *self {
             RWError::CsvError => HttpResponse::InternalServerError().finish(),
+            RWError::PoolError(ref err) => {
+                HttpResponse::InternalServerError().body(err.to_string())
+            }
+            RWError::PGError(ref err) => HttpResponse::InternalServerError().body(err.to_string()),
+            RWError::PGMError(ref err) => HttpResponse::InternalServerError().body(err.to_string()),
         }
     }
 }
